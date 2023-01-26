96886129ca97fa50cebea17c0712613d0fc3856b
YounesEch <114940175+YounesEch@users.noreply.github.com>
2023-01-26 21:56:45 +0100

    fix med cookies

--- main.js
+++ main.js
@@ -5,9 +5,23 @@ hamburger.addEventListener('click', menuOpen);
 navMenu.addEventListener('click', navOpen);
 
 function menuOpen () {;
-    navMenu.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  hamburger.classList.remove('active');
 }
 
 function navOpen(){
-  navMenu.classList.toggle('active');
+  navMenu.classList.remove('active');
+  hamburger.classList.toggle('active');
+}
+
+
+const cookieBox = document.querySelector('.wrapper');
+const acceptBtn = document.querySelector('.accept');
+const rejectBtn = document.querySelector('.reject');
+
+acceptBtn.addEventListener('click', clearSite);
+
+function clearSite () {
+  cookieBox.classList.toggle('active');
+  console.log(wrapper);
 }
